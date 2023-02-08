4bdf8d8bd3fe0bd98f63f13fd88f546ad2e2d912
tawat-ki <tawat_ki@kkumail.com>
2023-02-08 16:25:03 +0700

    :(

--- keyboards/lily58/keymaps/chuen/keymap.c
+++ keyboards/lily58/keymaps/chuen/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS,  KC_EQL, XXXXXXX,
   KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGHT, XXXXXXX,
   KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_LEFT, XXXXXXX, XXXXXXX,  KC_END, XXXXXXX,
-_______,HYPR(KC_M), XXXXXXX, KC_DOWN,   KC_UP, XXXXXXX, TO(_D),  TO(_DVORAK), XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+_______,HYPR(KC_Z), XXXXXXX, KC_DOWN,   KC_UP, XXXXXXX, TO(_D),  TO(_DVORAK), XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 //GAME MODE
