83bd1298c9804489dbf76d3cab7b45c8e38b0eeb
tawat-ki <tawat.kiate@gmail.com>
2023-05-26 23:03:02 +0700

    keymap

--- keyboards/lily58/keymaps/chuen/keymap.c
+++ keyboards/lily58/keymaps/chuen/keymap.c
@@ -16,28 +16,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  ]   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  [   |
+ * |   -  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  [   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |BackSP|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|   -   |    |    =  |------+------+------+------+------+------|
+ * |   =  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|MO(GM) |    |MO(M)  |------+------+------+------+------+------|
  * |  FN  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCTL | LGUI |LShift| /Space  /       \Enter \  | ESC  | LAlt |MOUSE |
+ *                   | LCTL | LGUI |LShift| /Space  /       \Enter \  | ESC  | BSPC |LAlt  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_D] = LAYOUT(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-  KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_DEL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_GM), TO(_M),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                             KC_LCTL, KC_LGUI, KC_LSFT,  KC_SPC,  KC_ENT, KC_ESC,  MO(_FN), KC_RALT
+  KC_MINS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  KC_EQL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  MO(_FN), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_GM), TO(_M),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                             KC_LCTL, KC_LGUI, KC_LSFT, KC_SPC,  KC_ENT,  KC_ESC,  KC_BSPC, KC_LALT
 ),
 //FUNCTION
 [_FN] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  XXXXXXX,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGHT, XXXXXXX,
-  KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_LEFT, XXXXXXX, XXXXXXX, KC_END,  XXXXXXX,
+  KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   KC_DEL,  KC_LEFT, KC_TAB,  XXXXXXX, KC_END,  XXXXXXX,
   _______, XXXXXXX, XXXXXXX, KC_DOWN, KC_UP,   XXXXXXX, TO(_D),  TO(_DV), XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
@@ -61,29 +61,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  =   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   '  |   ,  |   .  |   p  |   y  |                    |   f  |   g  |   c  |   r  |   l  |  /   |
+ * |   [  |   '  |   ,  |   .  |   p  |   y  |                    |   f  |   g  |   c  |   r  |   l  |  /   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |BackSP|   a  |   o  |   e  |   u  |   i  |-------.    ,-------|   d  |   h  |   t  |   n  |   s  |  -   |
- * |------+------+------+------+------+------| xx[xx |    | xx]xx |------+------+------+------+------+------|
+ * |   ]  |   a  |   o  |   e  |   u  |   i  |-------.    ,-------|   d  |   h  |   t  |   n  |   s  |  -   |
+ * |------+------+------+------+------+------|MO(GM) |    |MO(M)  |------+------+------+------+------+------|
  * |  FN  |   ;  |   q  |   j  |   k  |   x  |-------|    |-------|   b  |   m  |   w  |   v  |   z  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCTL | LGUI |LShift| /Space  /       \Enter \  | ESC  | LAlt |MOUSE |
+ *                   | LCTL | LGUI |LShift| /Space  /       \Enter \  | ESC  | BSPC |LAlt  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_DV] = LAYOUT(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQUAL,
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
-  KC_BSPC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
-  KC_DEL,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    TO(_GM), TO(_M),  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_BSLS,
-                             KC_LCTL, KC_LGUI, KC_LSFT, KC_SPC,  KC_ENT,  KC_ESC,  MO(_FD), KC_LALT
+  KC_LBRC, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+  KC_RBRC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+  MO(_FN), KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    TO(_GM), TO(_M),  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_BSLS,
+                             KC_LCTL, KC_LGUI, KC_LSFT, KC_SPC,  KC_ENT,  KC_ESC,  KC_BSPC, KC_LALT
 ),
 
 //FUNCTION_DVORACK
 [_FD] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGHT, XXXXXXX,
-  KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_LEFT, XXXXXXX, XXXXXXX, KC_END,  XXXXXXX,
+  KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   KC_DEL,  KC_LEFT, KC_TAB,  XXXXXXX, KC_END,  XXXXXXX,
   _______, XXXXXXX, XXXXXXX, KC_DOWN, KC_UP,   XXXXXXX, TO(_D),  TO(_DV), XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______, _______, _______, _______
 )
