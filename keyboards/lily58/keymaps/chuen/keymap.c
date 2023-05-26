0d90f76cc05f4c8a45262548423c53e0c9d5f077
tawat-ki <tawat.kiate@gmail.com>
2023-05-26 15:50:07 +0700

    alignment

--- keyboards/lily58/keymaps/chuen/keymap.c
+++ keyboards/lily58/keymaps/chuen/keymap.c
@@ -5,8 +5,8 @@ enum layer_number {
   _FN,
   _M,
   _GM,
-  _DVORAK,
-  _FN_DVORAK,
+  _DV,
+  _FD,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -22,40 +22,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   -   |    |    =  |------+------+------+------+------+------|
  * |  FN  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LCTRL | LGUI |LShift| /Space  /       \Enter \  | ESC  | LAlt |MOUSE |
+ *                   | LCTL | LGUI |LShift| /Space  /       \Enter \  | ESC  | LAlt |MOUSE |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_D] = LAYOUT(
-  KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_RBRC,
-  KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
-  KC_BSPC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  MO(_FN),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, TO(_GM),  TO(_M),    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
-                            KC_LCTRL, KC_LGUI, KC_LSFT,  KC_SPC,  KC_ENT,  KC_ESC, KC_LALT,  KC_DEL
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_DEL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_GM), TO(_M),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                             KC_LCTL, KC_LGUI, KC_LSFT,  KC_SPC,  KC_ENT, KC_ESC,  MO(_FN), KC_RALT
 ),
 //FUNCTION
 [_FN] = LAYOUT(
-  KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS,  KC_EQL, XXXXXXX,
-  KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGHT, XXXXXXX,
-  KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_LEFT, XXXXXXX, XXXXXXX,  KC_END, XXXXXXX,
-_______,HYPR(KC_Z), XXXXXXX, KC_DOWN,   KC_UP, XXXXXXX, TO(_D),  TO(_DVORAK), XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  XXXXXXX,
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGHT, XXXXXXX,
+  KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_LEFT, XXXXXXX, XXXXXXX, KC_END,  XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, KC_DOWN, KC_UP,   XXXXXXX, TO(_D),  TO(_DV), XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 //GAME MODE
   [_GM] = LAYOUT(
-  KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX,  TO(_D),
-  KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_BSPC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   DM_REC1, DM_REC2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_SPC,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, TO(_GM),  TO(_M), DM_PLY1, DM_PLY2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                            KC_LCTRL,  KC_GRV, KC_LSFT, KC_LALT, DM_RSTP,  TO(_D), XXXXXXX, XXXXXXX
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, TO(_D),
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      DM_REC1, DM_REC2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_SPC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_GM), TO(_M),  DM_PLY1, DM_PLY2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                             KC_LCTL, KC_GRV,  KC_LSFT, KC_LALT, DM_RSTP, TO(_D),  XXXXXXX, XXXXXXX
 ),
 //MOUSE
 [_M] = LAYOUT(
-  XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(_D),
+  XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TO(_D),
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_R, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_D, KC_WH_U, XXXXXXX,                   XXXXXXX, KC_MS_L, XXXXXXX, XXXXXXX, KC_WH_R, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, KC_MS_D, KC_MS_U, XXXXXXX, TO(_GM),  TO(_M), XXXXXXX, KC_WH_L, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                             _______, _______, _______, KC_BTN1, KC_BTN2,  TO(_D), _______, _______
+  _______, XXXXXXX, XXXXXXX, KC_MS_D, KC_MS_U, XXXXXXX, TO(_GM), TO(_M),  XXXXXXX, KC_WH_L, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                             _______, _______, _______, KC_BTN1, KC_BTN2, TO(_D),  _______, _______
 ),
 /*dvorak
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -67,24 +67,24 @@ _______,HYPR(KC_Z), XXXXXXX, KC_DOWN,   KC_UP, XXXXXXX, TO(_D),  TO(_DVORAK), XX
  * |------+------+------+------+------+------| xx[xx |    | xx]xx |------+------+------+------+------+------|
  * |  FN  |   ;  |   q  |   j  |   k  |   x  |-------|    |-------|   b  |   m  |   w  |   v  |   z  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LCTRL | LGUI |LShift| /Space  /       \Enter \  | ESC  | LAlt |MOUSE |
+ *                   | LCTL | LGUI |LShift| /Space  /       \Enter \  | ESC  | LAlt |MOUSE |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-[_DVORAK] = LAYOUT(
-  KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_EQUAL,
-  KC_TAB,     KC_QUOT,    KC_COMM,    KC_DOT,    KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH,
-  KC_BSPC,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N, KC_S, KC_MINS,
-  MO(_FN_DVORAK),    KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X, TO(_GM),  TO(_M),    KC_B,    KC_M, KC_W,  KC_V, KC_Z, KC_BSLS,
-                            KC_LCTRL, KC_LGUI, KC_LSFT,  KC_SPC,  KC_ENT,  KC_ESC, KC_LALT,  KC_DEL
+[_DV] = LAYOUT(
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQUAL,
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+  KC_BSPC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+  KC_DEL,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    TO(_GM), TO(_M),  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_BSLS,
+                             KC_LCTL, KC_LGUI, KC_LSFT, KC_SPC,  KC_ENT,  KC_ESC,  MO(_FD), KC_LALT
 ),
 
 //FUNCTION_DVORACK
-[_FN_DVORAK] = LAYOUT(
-  KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC,  KC_RBRC, XXXXXXX,
-  KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGHT, XXXXXXX,
-  KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_LEFT, XXXXXXX, XXXXXXX,  KC_END, XXXXXXX,
-_______,HYPR(KC_M), XXXXXXX, KC_DOWN,   KC_UP, XXXXXXX, TO(_D),  TO(_DVORAK), XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+[_FD] = LAYOUT(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGHT, XXXXXXX,
+  KC_CAPS, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_LEFT, XXXXXXX, XXXXXXX, KC_END,  XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, KC_DOWN, KC_UP,   XXXXXXX, TO(_D),  TO(_DV), XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______, _______, _______, _______
 )
 };
