ef85a741c11a1454a3d5423659e32ba6c279e17a
tawat-ki <tawat.kiate@gmail.com>
2023-09-15 12:45:47 +0700

    idk

--- keyboards/lily58/keymaps/chuen/keymap.c
+++ keyboards/lily58/keymaps/chuen/keymap.c
@@ -6,6 +6,8 @@ enum layer_number {
   _R,
   _M,
   _N,
+  _G,
+  _K,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -31,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  MO(_N), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_N),  TO(_M), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+  MO(_N), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_G),  TO(_M), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
                              KC_LGUI, MO(_L),  KC_LSFT,  KC_SPC,  KC_ENT, MO(_R),  KC_RGUI, KC_LALT
 ),
 
@@ -56,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, TO(_D),  XXXXXXX, XXXXXXX, KC_RGHT, TO(_D),
   _______, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_LEFT, XXXXXXX, XXXXXXX, KC_END,  XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, KC_DOWN, KC_UP,   XXXXXXX, TO(_M),  TO(_M),  XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, KC_DOWN, KC_UP,   XXXXXXX, TO(_G),  TO(_M),  XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 //MOUSE
@@ -64,11 +66,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_R, TO(_D),
   _______, XXXXXXX, XXXXXXX, KC_WH_D, KC_WH_U, XXXXXXX,                   XXXXXXX, KC_MS_L, XXXXXXX, XXXXXXX, KC_WH_R, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, KC_MS_D, KC_MS_U, XXXXXXX, TO(_M), TO(_M),  XXXXXXX, KC_WH_L, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, KC_MS_D, KC_MS_U, XXXXXXX, TO(_G),  TO(_M),  XXXXXXX, KC_WH_L, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, KC_BTN1, KC_BTN2, MO(_N), _______, _______
 ),
-};
 
+
+[_G] = LAYOUT(
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      TO(_K),  TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      TO(_K),  TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),
+  KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      TO(_K),  TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),
+  KC_SPC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_G),   TO(_K), TO(_K),  TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),
+                             KC_RCTL, KC_GRV,  KC_LSFT, KC_LALT,  TO(_D), TO(_D),  TO(_D),  TO(_D)
+),
+[_K] = LAYOUT(
+  KC_ESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                      TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),
+  KC_F12,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),
+  KC_F11, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),
+  KC_F10,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_K),   TO(_K), TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),  TO(_D),
+                             KC_RCTL, KC_F9,  KC_LSFT, KC_LALT,  TO(_D), TO(_D),  TO(_D),  TO(_D)
+),
+};
 
 #ifdef OLED_ENABLE
 
